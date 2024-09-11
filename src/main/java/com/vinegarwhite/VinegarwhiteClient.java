9112b19cb147f180eaaf760f603dba6c080a660f
rageniv <1311763909@qq.com>
2024-09-11 17:21:22 +0800

    Initial commit

---
+++ src/main/java/com/vinegarwhite/VinegarwhiteClient.java
@@ -0,0 +1,10 @@
+package com.vinegarwhite;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VinegarwhiteClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
